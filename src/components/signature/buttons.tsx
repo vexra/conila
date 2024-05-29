90ab14f0e912842c2bdae40a437ce17e9c6ae730
vexra <unique123wowros@gmail.com>
2024-05-29 08:58:36 +0700

    fix: fix qr code download

--- src/components/signature/buttons.tsx
+++ src/components/signature/buttons.tsx
@@ -2,7 +2,6 @@
 
 import { deleteSignatureById } from '@/actions/signature'
 import QRCode from 'qrcode'
-import { useEffect, useRef } from 'react'
 
 export function DeleteSignature({
   id,
@@ -28,46 +27,23 @@ type DownloadSignatureQRCodeProps = {
 }
 
 export function DownloadSignatureQRCode({ id }: DownloadSignatureQRCodeProps) {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const downloadQRCode = async () => {
+    try {
+      const qrCodeDataURL = await QRCode.toDataURL(
+        `${location.origin}/signatures/${id}`,
+        {
+          width: 300,
+          margin: 2,
+          errorCorrectionLevel: 'H',
+        },
+      )
 
-  useEffect(() => {
-    const generateQRCode = async () => {
-      const canvas = canvasRef.current
-      const context = canvas?.getContext('2d')
-
-      if (canvas && context) {
-        try {
-          const qrCode = await QRCode.toDataURL(
-            `${location.origin}/signatures/${id}`,
-            {
-              width: 300,
-              margin: 2,
-              errorCorrectionLevel: 'H',
-            },
-          )
-
-          const qrCodeImg = new Image()
-          qrCodeImg.src = qrCode
-
-          qrCodeImg.onload = () => {
-            context.drawImage(qrCodeImg, 0, 0, canvas.width, canvas.height)
-          }
-        } catch (error) {
-          console.error('Error generating QR code:', error)
-        }
-      }
-    }
-
-    generateQRCode()
-  }, [id])
-
-  const downloadQRCode = () => {
-    const canvas = canvasRef.current
-    if (canvas) {
       const anchor = document.createElement('a')
       anchor.download = 'qr-code.png'
-      anchor.href = canvas.toDataURL('image/png')
+      anchor.href = qrCodeDataURL
       anchor.click()
+    } catch (error) {
+      console.error('Error generating and downloading QR code:', error)
     }
   }
 
